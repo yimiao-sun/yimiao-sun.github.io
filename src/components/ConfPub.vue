d1706a84fd22f45dbc97019219657ff81186937e
Yimiao Sun <126239016+yimiao-sun@users.noreply.github.com>
2023-08-10 10:43:00 +0800

    111

--- src/components/ConfPub.vue
+++ src/components/ConfPub.vue
@@ -104,7 +104,7 @@ function copyToClipboard(text, pubId, cslTemplateType) {
 </script>
 
 <template>
-  <h2  style="font-size: 20pt">Publications</h2>
+  <h2  style="font-size: 20pt">Conference Articles</h2>
   <!-- <p>
     <b>bold</b>: myself.
     <sup>*</sup>: corresponding author(s).
