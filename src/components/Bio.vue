c57dad564d6d00dfa46ea3211770a3e8ca1d5539
Yimiao Sun <126239016+yimiao-sun@users.noreply.github.com>
2025-07-11 17:49:48 +0800

    award update

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -16,7 +16,7 @@ const description = computed(() => {
       <div style="font-size: 12pt" v-html="description"></div>
     </div>
     <div>
-      <img class="col" src="../assets/personal-photo.jpg" width="162" alt="personal photo">
+      <img class="col" src="../assets/photo.jpg" width="162" alt="personal photo">
     </div>
   </div>
   <div class="row flex-start">
